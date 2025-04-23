92008e1a9c75a753e9acdc1c24cb780ee23ed662
1pharaxh <pharaohsupreme03@gmail.com>
2025-04-23 10:51:57 +0530

    feat: implement DrawView component for drawing functionality in ImageView

---
+++ components/ui/DrawView.tsx
@@ -0,0 +1,52 @@
+import { Skia, SkPath } from "@shopify/react-native-skia";
+import React from "react";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import {
+  DerivedValue,
+  SharedValue,
+  useDerivedValue,
+  useSharedValue,
+} from "react-native-reanimated";
+
+type Props = {
+  children: React.ReactNode;
+  currentPath: SharedValue<SkPath>;
+};
+
+const DrawView = ({ children, currentPath }: Props) => {
+  const currentPathObject = useSharedValue(Skia.Path.Make());
+  const currentPathX = useSharedValue(0);
+  const currentPathY = useSharedValue(0);
+  const isStartDrawing = useSharedValue(false);
+
+  useDerivedValue(() => {
+    const newPath = currentPathObject.value.copy();
+    if (isStartDrawing.value) {
+      newPath.moveTo(currentPathX.value, currentPathY.value);
+      newPath.lineTo(currentPathX.value, currentPathY.value);
+    } else if (currentPathX.value !== 0 && currentPathY.value !== 0) {
+      newPath.lineTo(currentPathX.value, currentPathY.value);
+    }
+    currentPathObject.value = newPath;
+
+    currentPath.value = newPath;
+  });
+
+  const pan = Gesture.Pan()
+    .averageTouches(true)
+    .maxPointers(1)
+    .onBegin((e) => {
+      currentPathX.value = e.x;
+      currentPathY.value = e.y;
+      isStartDrawing.value = true;
+    })
+    .onChange((e) => {
+      currentPathX.value = e.x;
+      currentPathY.value = e.y;
+      isStartDrawing.value = false;
+    });
+
+  return <GestureDetector gesture={pan}>{children}</GestureDetector>;
+};
+
+export default DrawView;
--- components/ui/MediaView/ImageView.tsx
+++ components/ui/MediaView/ImageView.tsx
@@ -3,13 +3,18 @@ import {
   Canvas,
   CanvasProps,
   ColorMatrix,
+  DiscretePathEffect,
   Image,
+  Path,
+  Skia,
   useImage,
 } from "@shopify/react-native-skia";
 import useGlobalStore from "~/store/globalStore";
 import { GestureHandler } from "../GestureHandler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import RenderStickers from "../Stickers/RenderStickers";
+import { useSharedValue } from "react-native-reanimated";
+import DrawView from "../DrawView";
 
 type ImageViewProps = Omit<CanvasProps, "children"> & {};
 
@@ -19,39 +24,49 @@ export default function ImageView(props: ImageViewProps) {
   const image = useImage(photo);
   const { width, height } = Dimensions.get("window");
 
+  const currentPath = useSharedValue(Skia.Path.Make());
+
   return (
     <GestureHandlerRootView style={{ flex: 1, position: "relative" }}>
-      <Canvas style={{ flex: 1 }} {...rest}>
-        <Image
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          image={image}
-          fit="cover"
-        />
-        {/* if you want text color to not change then move color matrix inside image  */}
-        <ColorMatrix
-          matrix={
-            filter && filter.colorMatrix.length > 0
-              ? filter.colorMatrix
-              : [
-                  1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                  1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-                ]
-          }
-        />
+      <DrawView currentPath={currentPath}>
+        <View style={{ flex: 1 }}>
+          <Canvas style={{ flex: 1 }} {...rest}>
+            <Image
+              x={0}
+              y={0}
+              width={width}
+              height={height}
+              image={image}
+              fit="cover"
+            />
+            <ColorMatrix
+              matrix={
+                filter && filter.colorMatrix.length > 0
+                  ? filter.colorMatrix
+                  : [
+                      1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                    ]
+              }
+            />
+            {stickers?.map((e, idx) => (
+              <RenderStickers item={e} matrix={e.matrix} key={idx} />
+            ))}
+            <Path
+              path={currentPath}
+              color="#61DAFB"
+              style="stroke"
+              strokeWidth={2}
+            >
+              <DiscretePathEffect length={10} deviation={2} />
+            </Path>
+          </Canvas>
 
-        {/* Render text at center of screen */}
-        {stickers?.map((e, idx) => (
-          <RenderStickers item={e} matrix={e.matrix} key={idx} />
-        ))}
-      </Canvas>
-
-      {/* Position gesture handler at the same position as text */}
-      {stickers?.map((e, idx) => (
-        <GestureHandler debug key={idx} sticker={e} />
-      ))}
+          {stickers?.map((e, idx) => (
+            <GestureHandler debug key={idx} sticker={e} />
+          ))}
+        </View>
+      </DrawView>
     </GestureHandlerRootView>
   );
 }
